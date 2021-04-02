512c510bd9da422c6395b3dcffb2e799ec0de0aa
Will <32076464+wdsides@users.noreply.github.com>
2021-04-02 01:26:12 -0700

    Add files via upload

---
+++ private_API_keys.py
@@ -0,0 +1,5 @@
+twilio_sid='' #create a free Twilio account and copy your API keys here
+twilio_auth_token='' 
+phonenumber='+1' #the phone number (format = '+1##########') to send text notifications to (must be the same as the one you signed up to Twilio with for free trial users)
+twilionumber='+1' #the Twilio phone number (format = '+1##########') associated with your account
+geocoder_key='' #Create a free account at opencagedata.com and copy your API key here
